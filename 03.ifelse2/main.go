e7fbb0a738d807e82d317a4c1a97fde861e9c55a
zeitlos <adasheng007@gmail.com>
2021-12-07 16:55:05 +0800

    else-if nested syntax

--- 03.ifelse2/main.go
+++ 03.ifelse2/main.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
+/*
+else-if语句嵌套语法：
+	if 布尔表达式 1 {
+   		// 在布尔表达式 1 为 true 时执行
+	} else if 布尔表达式 2 {
+		// 在布尔表达式 1 为 false 且布尔表达式 2 为 true 时执行
+	} else if 布尔表达式 3 {
+		// 在布尔表达式 1 为 false, 布尔表达式 2 为 false, 且布尔表达式 3 为 true 时执行
+	} else {
+		// 在布尔表达式 1,2,3 均为 false 时执行
+	}
+*/
 func main() {
 	var money int
 	if money >= 20 {
