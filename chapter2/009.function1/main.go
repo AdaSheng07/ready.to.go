19b10c590fe44e51ee5487b9f868712852197312
zeitlos <adasheng007@gmail.com>
2021-12-21 16:58:51 +0800

    fixed-length & unfixed-length parameters & named return values

--- chapter2/009.function1/main.go
+++ chapter2/009.function1/main.go
@@ -4,14 +4,20 @@ import (
 	"fmt"
 )
 
+// bmi is declared outside, valid both outside and inside of main() and other functions
+var bmi []float64
+
 func main() {
+	// total is declared in main() first
+	var total float64
+	fmt.Println("declare total in main() first: total =", total)
 	// fixed-length & unfixed-length parameters
 	helloToOnePerson := constructHello1("Tom")
 	helloToPersons := constructHello2("Tom", "David")
 	fmt.Println(helloToOnePerson)
 	fmt.Println(helloToPersons)
 
-	bmi := []float64{0.203, 0.242, 0.261}
+	bmi = []float64{0.203, 0.242, 0.261}
 	aveBmi1 := calculateAverage1(bmi)
 	fmt.Println("Average BMI:", aveBmi1)
 
@@ -24,13 +30,13 @@ func main() {
 	// naming return values is a good habit
 	// 1. named return values can be used directly in main function
 	// 2. named return values also enables people who use this piece of code understand it quickly
-	sexOfStudent, ageOfStudent, heightOfStudent := getInfoOfStudents("Tom")
-	fmt.Println("Tom:\nsex:", sexOfStudent, "\nage:", ageOfStudent, "\nheight:", heightOfStudent, "m")
+	nameOfStudent, sexOfStudent, ageOfStudent, heightOfStudent := getInfoOfStudents("Tom")
+	fmt.Println(nameOfStudent, "\nsex:", sexOfStudent, "\nage:", ageOfStudent, "\nheight:", heightOfStudent, "m")
 
 }
 
-func getInfoOfStudents(stdName string) (sex string, age int, height float64) {
-	return "Male", 18, 1.70
+func getInfoOfStudents(stdName string) (name string, sex string, age int, height float64) {
+	return stdName, "Male", 18, 1.70
 }
 
 func constructHello1(name string) string {
@@ -40,7 +46,10 @@ func constructHello2(name ...string) string {
 	return fmt.Sprintf("hello, %s", name)
 }
 
+// variable total is both in calculateAverage1 and calculateAverage2
+// it is okay to do that because they are different blocks and have different scopes
 func calculateAverage1(nums []float64) float64 {
+	// redeclare total in another scope, it will cover the total from main()
 	var total float64 = 0
 	for _, num := range nums {
 		total += num
