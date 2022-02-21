e9660173dc3a180fe76efdae4b59f4cd6cffaec9
zeitlos <adasheng007@gmail.com>
2022-02-21 12:02:18 +0800

    update homework 4

---
+++ 000.homework/0005.1000FatRateList/main.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sort"
+	"strconv"
+	"sync"
+	"time"
+)
+
+type Person struct {
+	Name    string
+	FatRate float64
+	Rank    int
+	lock    sync.Mutex
+}
+
+type FatRateList struct {
+	FatRates []float64
+	lock     sync.RWMutex
+}
+
+func (p *Person) GetRank(name string, persons []Person, fatRates []float64) (rank int) {
+	p.lock.Lock()
+	defer p.lock.Unlock()
+	sort.Float64s(fatRates)
+	i, _ := strconv.Atoi(name)
+	return sort.SearchFloat64s(fatRates, persons[i].FatRate)
+}
+
+func main() {
+
+	// 初始化数据
+	personCount := 1000 // 一共有1000人
+	persons := []Person{}
+	fatRateList := &FatRateList{
+		FatRates: []float64{},
+	} // 1000人的体脂率
+
+	// 生成随机数base，base范围在[0, 0.4]
+	rand.Seed(time.Now().UnixNano())
+	base := rand.Float64() * 0.4
+
+	for i := 0; i < personCount; i++ {
+		persons = append(persons, Person{
+			Name: fmt.Sprintf("%d", i),
+		})
+		for {
+			rand.Seed(time.Now().UnixNano())
+			delta := rand.Float64()*0.4 - 0.2 // 体脂率波动范围[-0.2, 0.2]
+			// 保证体脂率是非负数，防止非法输入
+			if base+delta >= 0 {
+				persons[i].FatRate = base + delta
+				fatRateList.FatRates = append(fatRateList.FatRates, base+delta)
+				break
+			}
+		}
+	}
+
+	fmt.Println("查询排名")
+	count := 1000
+	for c := 0; c < count; c++ {
+		// 查询排名
+		go func(c int) {
+			fmt.Println(persons[c].Name, "号开始查询排名：")
+			persons[c].lock.Lock()
+			fatRateList.lock.Lock()
+			sort.Float64s(fatRateList.FatRates)
+			fatRateList.lock.Unlock()
+			index, _ := strconv.Atoi(persons[c].Name)
+			persons[c].Rank = sort.SearchFloat64s(fatRateList.FatRates, persons[index].FatRate)
+			fmt.Println(persons[c].Name, "号的体脂率排名为：", persons[c].Rank)
+			persons[c].lock.Unlock()
+		}(c)
+	}
+
+	fmt.Println("更新并打印排名")
+	count := 1000
+	for c := 0; c < count; c++ {
+		// 查询排名
+		go func(c int) {
+			fmt.Println(persons[c].Name, "号开始查询排名：")
+			persons[c].lock.Lock()
+			fatRateList.lock.Lock()
+			sort.Float64s(fatRateList.FatRates)
+			fatRateList.lock.Unlock()
+			index, _ := strconv.Atoi(persons[c].Name)
+			persons[c].Rank = sort.SearchFloat64s(fatRateList.FatRates, persons[index].FatRate)
+			fmt.Println(persons[c].Name, "号的体脂率排名为：", persons[c].Rank)
+			persons[c].lock.Unlock()
+		}(c)
+	}
+
+	time.Sleep(5 * time.Second)
+}
